eac2efdfb229e13fcea8c03e683bebd48939dee2
Loanshark <63855173+changhyuni@users.noreply.github.com>
2020-11-10 22:06:07 +0900

    Create k8s-autocomplet.sh

---
+++ etc/k8s-autocomplet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+alias k=kubectl
+complete -F __start_kubectl k
+source <(kubectl completion zsh)  # 현재 셸에 zsh의 자동 완성 설정
+echo "[[ $commands[kubectl] ]] && source <(kubectl completion zsh)" >> ~/.zshrc
